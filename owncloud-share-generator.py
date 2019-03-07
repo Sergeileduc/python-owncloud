76994eb49633f876b370519783730121da302a1b
Sergeileduc <sergei.leduc@gmail.com>
2019-03-07 12:20:56 +0100

    Navigation

--- owncloud-share-generator.py
+++ owncloud-share-generator.py
@@ -5,6 +5,7 @@ import configparser
 import owncloud  # pip install pyocclient
 # import urllib.request as urllib2  # Python3
 from urllib.error import HTTPError
+import requests.exceptions
 import importlib
 import sys
 
@@ -47,20 +48,30 @@ if cfg.load(cfg_forum, cfg_opts):
     # oc = owncloud.Client('http://dl.dctrad.fr')
     oc = owncloud.Client(cfg.host)
     # oc.login('Staff', 'staff123')
-    oc.login(cfg.username, cfg.password)
+    try:
+        oc.login(cfg.username, cfg.password)
+    except requests.exceptions.MissingSchema:
+        print("Erreur.\n"
+              "Veuillez configurer owncloud.cfg avec une url correcte")
+        sys.exit(1)
+    except owncloud.owncloud.HTTPResponseError as e:
+        print("Erreur.\n"
+              "Veuillez configurer owncloud.cfg avec utilisateur "
+              "et mot de passe valide")
+        # print(e)
+        sys.exit(1)
 
     print ('--------------------------------------------------------------')
     print ('      Entrez (ou collez) le chemin vers le dossier Owncloud: ')
     print ('      Exemple : DC Comics/New 52/Groupe Batman/Batman')
     print ('--------------------------------------------------------------')
 
-    try:
-        folder = raw_input()
-    except Exception as e:
-        folder = input()
+    # try:
+    #     folder = raw_input()
+    # except Exception as e:
+    #     folder = input()
 
     try:
-        list_dir = oc.list(folder)
         print ('-------------------------------------------------------------')
         print (r'         ____                      __                __')
         print (r'        / __ \_      ______  _____/ /___  __  ______/ /')
@@ -70,7 +81,56 @@ if cfg.load(cfg_forum, cfg_opts):
                oc.get_version()+'')
         print ('                                              ')
         print ('-------------------------------------------------------------')
+    except Exception:
+        sys.exit(1)
 
+    try:
+        folder_path = '/'
+        folder_name = ''
+        while True:
+            list_dir = oc.list(folder_path, depth=1)
+            print("==========================================================")
+            print("Contenu de : " + folder_path)
+            print("Dossiers")
+            print("..........................................................")
+            i = 1
+            folder_list = []
+            for file in list_dir:
+                if file.is_dir():
+                    name = file.get_name()
+                    # full_path = file.get_path() + '/' + newName
+                    full_path = file.get_path()
+                    folder_list.append((full_path, name))
+                    # print(newName)
+                    print(str(i) + "-\t" + full_path)
+                    i += 1
+            print("==========================================================")
+            print("Fichiers")
+            print("..........................................................")
+            for file in list_dir:
+                if not file.is_dir():
+                    newName = file.get_name()
+                    full_path = file.get_path() + '/' + newName
+                    shared = 'no'
+                    if oc.is_shared(full_path):
+                        shared = 'yes'
+                    print(newName + '\t' + 'Partage : ' + shared)
+            print("==========================================================")
+            choice = input("tapez un chiffre pour naviguer dans "
+                           "le dossier, "
+                           "ou 'y' pour créer les liens de partage.\n")
+            if choice != 'y':
+                try:
+                    folder_path = folder_list[int(choice)-1][0]
+                    folder_name = folder_list[int(choice)-1][1]
+                except IndexError:
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    print("Mauvaise valeur")
+
+            else:
+                break
+
+        list_dir = oc.list(folder_path)
         for files in list_dir:
             newName = files.get_name()
             path = files.get_path() + '/' + newName
